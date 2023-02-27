0fe241e0931f0683ee0fe15fd0b2830aafab3040
Luca <luca.mlemos@gmail.com>
2023-02-27 14:48:59 -0300

    correção de pixel

--- main.c
+++ main.c
@@ -2,14 +2,15 @@
 #include "raylib.h"
 #include "lerArquivo.c"
 
-const int screenWidth = 1600;
-const int screenHeight = 900;
+const int screenWidth = 800;
+const int screenHeight = 450;
 const int linhaDiv = 15;
 
-void desenhaLinha(float P1x, float P1y, float P2x, float P2y, Color COR);
-void desenha(float teste[3][3][10000]);
-void coordenadaTela(float parametros[3][3][10000], float camera[3][6]);
+void ordena(float pontoCima[2], float pontoMedio[2], float pontoBaixo[2]);
 void rasterizar(float P1x, float P1y, float P2x, float P2y, float P3x, float P3y);
+void desenhaLinha(float P1x, float P1y, float P2x, float P2y, Color COR);
+void coordenadaTela(float parametros[3][3][10000], float camera[3][6]);
+void desenha(float teste[3][3][10000]);
 
 int main(void) {
     InitWindow(screenWidth, screenHeight, "Projeto Computação Gráfica");
@@ -81,10 +82,7 @@ void desenha(float teste[3][3][10000]) {
         triangulos[i] = (Vector3){ teste[2][0][i], teste[2][1][i], teste[2][2][i] };    
     }
 
-    for (int i = 0; i < numVertices; i++) {
-        //DrawPixel(vertices[i].x, vertices[i].y, GREEN);
-    }
-
+    
     for (int i = 0; i < numTriangulos; i++) {
         int ver1 = triangulos[i].x - 1;
         int ver2 = triangulos[i].y - 1;
@@ -92,6 +90,14 @@ void desenha(float teste[3][3][10000]) {
 
         rasterizar(vertices[ver1].x, vertices[ver1].y, vertices[ver2].x, vertices[ver2].y, vertices[ver3].x, vertices[ver3].y);
         
+        //desenhaLinha(vertices[ver1].x, vertices[ver1].y, vertices[ver2].x, vertices[ver2].y, YELLOW);
+        //desenhaLinha(vertices[ver1].x, vertices[ver1].y, vertices[ver3].x, vertices[ver3].y, YELLOW);
+        //desenhaLinha(vertices[ver2].x, vertices[ver2].y, vertices[ver3].x, vertices[ver3].y, YELLOW);
+
+    }
+
+    for (int i = 0; i < numVertices; i++) {
+        //DrawPixel(vertices[i].x, vertices[i].y, GREEN);
     }
 
 }
@@ -263,14 +269,145 @@ void coordenadaTela(float parametros[3][3][10000], float camera[3][6]) {
 }
 
 void rasterizar(float P1x, float P1y, float P2x, float P2y, float P3x, float P3y) {
-    float ponto1X = P1x;
-    float ponto1Y = P1y;
-    float ponto2X = P2x;
-    float ponto2Y = P2y;
-    float ponto3X = P3x;
-    float ponto3Y = P3y;
+    float pontoCima[2] = {P1x, P1y};
+    float pontoMedio[2] = {P2x, P2y};
+    float pontoBaixo[2] = {P3x, P3y};
 
-    if (ponto1Y > ponto3Y) {
+//Ordena os pontos do mais alto para o mais baixo
+    ordena(pontoCima, pontoMedio, pontoBaixo);
+
+    printf("PontoCima X: %f PontoCima Y: %f\n", pontoCima[0], pontoCima[1]);
+    printf("pontoMedio X: %f pontoMedio Y: %f\n", pontoMedio[0], pontoMedio[1]);
+    printf("pontoBaixo X: %f pontoBaixo Y: %f\n\n", pontoBaixo[0], pontoBaixo[1]);
+
+    float Yscan = pontoCima[1];
+    float Xmin = pontoCima[0];
+    float Xmax = pontoCima[0];
+
+    float aEsq;
+    float aDir;
+
+//garante uma inclinação valida 
+    float temp = (pontoMedio[0] - pontoCima[0]);
+    if (temp == 0) {
+        aEsq = 0;
+    }else {
+        aEsq = (int) (pontoMedio[1] - pontoCima[1])/(pontoMedio[0] - pontoCima[0]); 
+        if (!aEsq == 0) {
+            aEsq = (1/aEsq);
+        }
+    }
+    temp = (pontoBaixo[0] - pontoCima[0]);
+    if (temp == 0) {
+        aDir = 0;
+    }else {
+        aDir = (int) (pontoBaixo[1] - pontoCima[1])/(pontoBaixo[0] - pontoCima[0]);
+        if (!aDir == 0) {
+            aDir = (1/aDir);
+        }
+    }
+
+    if(pontoMedio[0] > pontoBaixo[0]) {
+        float troca = aEsq;
+        aEsq = aDir;
+        aDir = troca;
+    }
+    
+    printf("aEsq: %f aDir: %f\n\n", aEsq, aDir);
+
+    printf("Xmin: %f Xmax: %f\n\n", Xmin, Xmax);
+
+
+    if(aEsq == 0 && aDir == 0) {
+        Xmax = pontoBaixo[0];
+    }
+
+    while (Yscan <= pontoMedio[1]) {
+        float Xmin_ = (Xmin + aEsq);
+        float Xmax_ = (Xmax + aDir);
+
+        printf("Xmin: %f Xmax: %f\n", round(Xmin), round(Xmax));
+        printf("Xmin_: %f Xmax_: %f\n\n", round(Xmin_), round(Xmax_));
+        desenhaLinha(round(Xmin), Yscan, round(Xmax), Yscan, BLUE);
+
+        Yscan++;
+        Xmin = Xmin_;
+        Xmax = Xmax_;
+    }
+//----------------------------------------------------------------
+
+// define o ponto medio D
+    float pontoD[2];
+    if(pontoMedio[0] > pontoBaixo[0]) {
+        pontoD[0] = (Xmin - aEsq); 
+        pontoD[1] = pontoMedio[1];
+    }else {
+        pontoD[0] = (Xmax - aDir); 
+        pontoD[1] = pontoMedio[1];
+    }
+    printf("pontoD X: %f pontoD Y: %f\n\n", pontoD[0], pontoD[1]);
+
+    Yscan = pontoBaixo[1];
+    Xmin = pontoBaixo[0];
+    Xmax = pontoBaixo[0];
+
+    //garante uma inclinação valida 
+    temp = (pontoMedio[0] - pontoBaixo[0]);
+    if (temp == 0) {
+        aEsq = 0;
+    }else {
+        aEsq = (int) (pontoMedio[1] - pontoBaixo[1])/(pontoMedio[0] - pontoBaixo[0]); 
+        if (!aEsq == 0) {
+            aEsq = (1/aEsq);
+        }
+    }
+    temp = (pontoD[0] - pontoBaixo[0]);
+    if (temp == 0) {
+        aDir = 0;
+    }else {
+        aDir = (int) (pontoD[1] - pontoBaixo[1])/(pontoD[0] - pontoBaixo[0]);
+        if (!aDir == 0) {
+            aDir = (1/aDir);
+        }
+    }
+    printf("aEsq: %f aDir: %f\n", aEsq, aDir);
+    if(pontoMedio[0] > pontoD[0]) {
+        float troca = aEsq;
+        aEsq = aDir;
+        aDir = troca;
+    }
+
+    printf("aEsq: %f aDir: %f\n\n", aEsq, aDir);
+
+    printf("Xmin: %f Xmax: %f\n\n", Xmin, Xmax);
+
+    if(aEsq == 0 && aDir == 0) {
+        Xmax = pontoMedio[0];
+    }
+
+    while (Yscan >= pontoMedio[1]) {
+        float Xmin_ = (Xmin - aEsq);
+        float Xmax_ = (Xmax - aDir);
+
+        printf("2Xmin: %f Xmax: %f\n", round(Xmin), round(Xmax));
+        printf("2Xmin_: %f Xmax_: %f\n\n", round(Xmin_), round(Xmax_));
+        desenhaLinha(round(Xmin), Yscan, round(Xmax), Yscan, BLUE);
+
+        Yscan--;
+        Xmin = Xmin_;
+        Xmax = Xmax_;
+    }
+}
+
+void ordena(float pontoCima[2], float pontoMedio[2], float pontoBaixo[2]) {
+    float ponto1X = pontoCima[0];
+    float ponto1Y = pontoCima[1];
+    float ponto2X = pontoMedio[0];
+    float ponto2Y = pontoMedio[1];
+    float ponto3X = pontoBaixo[0];
+    float ponto3Y = pontoBaixo[1];
+
+    if(ponto1Y > ponto3Y) {
         float tmp = ponto3Y;
         ponto3Y = ponto1Y;
         ponto1Y = tmp;
@@ -279,7 +416,7 @@ void rasterizar(float P1x, float P1y, float P2x, float P2y, float P3x, float P3y
         ponto3X = ponto1X;
         ponto1X = tmp;
     }
-    if (ponto1Y > ponto2Y) {
+    if(ponto1Y > ponto2Y) {
         float tmp = ponto2Y;
         ponto2Y = ponto1Y;
         ponto1Y = tmp;
@@ -288,7 +425,7 @@ void rasterizar(float P1x, float P1y, float P2x, float P2y, float P3x, float P3y
         ponto2X = ponto1X;
         ponto1X = tmp;
     }
-    if (ponto2Y > ponto3Y) {
+    if(ponto2Y > ponto3Y) {
         float tmp = ponto3Y;
         ponto3Y = ponto2Y;
         ponto2Y = tmp;
@@ -298,105 +435,45 @@ void rasterizar(float P1x, float P1y, float P2x, float P2y, float P3x, float P3y
         ponto2X = tmp;
     }
 
-    float pontoCima[2] = {ponto1X, ponto1Y};
-    float pontoMedio[2] = {ponto2X, ponto2Y};
-    float pontoBaixo[2] = {ponto3X, ponto3Y};
+    if(ponto1Y == ponto3Y) {
+        if(ponto1X > ponto3X) {
+            float tmp = ponto3Y;
+            ponto3Y = ponto1Y;
+            ponto1Y = tmp;
 
-    float Yscan = pontoCima[1];
-    float Xmin = pontoCima[0];
-    float Xmax = pontoCima[0];
-
-    float a1;
-    float a2;
-
-    float temp = (pontoMedio[0] - pontoCima[0]);
-    if (temp == 0) {
-        a1 = 0;
-    }else {
-        a1 = (pontoMedio[1] - pontoCima[1])/(pontoMedio[0] - pontoCima[0]); 
-        if (!a1 == 0) {
-            a1 = (1/a1);
+            tmp = ponto3X;
+            ponto3X = ponto1X;
+            ponto1X = tmp;
         }
     }
-    temp = (pontoBaixo[0] - pontoCima[0]);
-    if (temp == 0) {
-        a2 = 0;
-    }else {
-        a2 = (pontoBaixo[1] - pontoCima[1])/(pontoBaixo[0] - pontoCima[0]);
-        if (!a2 == 0) {
-            a2 = (1/a2);
+    if(ponto1Y == ponto2Y) {
+        if(ponto1X > ponto2X) {
+            float tmp = ponto2Y;
+            ponto2Y = ponto1Y;
+            ponto1Y = tmp;
+
+            tmp = ponto2X;
+            ponto2X = ponto1X;
+            ponto1X = tmp;
+        }
+    }
+    if(ponto2Y == ponto3Y) {
+        if(ponto2X > ponto3X) {
+            float tmp = ponto3Y;
+            ponto3Y = ponto2Y;
+            ponto2Y = tmp;
+
+            tmp = ponto3X;
+            ponto3X = ponto2X;
+            ponto2X = tmp;
         }
     }
 
-    if(pontoMedio[0] > pontoBaixo[0]) {
-        float troca = a1;
-        a1 = a2;
-        a2 = troca;
-    }
-
-    float Xmin_;
-    float Xmax_;
-
-    while (Yscan <= pontoMedio[1]) {
-        Xmin_ = (Xmin + a1);
-        Xmax_ = (Xmax + a2);
-
-        desenhaLinha(Xmin, Yscan, Xmax, Yscan, WHITE);
-
-        Yscan++;
-        Xmin = Xmin_;
-        Xmax = Xmax_;
-    }
-    Xmin = (Xmin - a1);
-    Xmax = (Xmax - a2);
-
-    float pontoD[2];
-    if((int)pontoMedio[0] > (int)pontoBaixo[0]) {
-        pontoD[0] = Xmin; 
-        pontoD[1] = pontoMedio[1];
-    }else {
-        pontoD[0] = Xmax; 
-        pontoD[1] = pontoMedio[1];
-    }
-
-    Yscan = pontoBaixo[1];
-    Xmin = pontoBaixo[0];
-    Xmax = pontoBaixo[0];
-
-    temp = (pontoMedio[0] - pontoBaixo[0]);
-    if (temp == 0) {
-        a1 = 0;
-    }else {
-        a1 = (pontoMedio[1] - pontoBaixo[1])/(pontoMedio[0] - pontoBaixo[0]);
-        if (!a1 == 0) {
-            a1 = (1/a1);
-        } 
-    }
-    temp = (pontoD[0] - pontoBaixo[0]);
-
-    if (temp == 0) {
-        a2 = 0;
-    }else {
-        a2 = (pontoD[1] - pontoBaixo[1])/(pontoD[0] - pontoBaixo[0]); 
-        if (!a2 == 0) {
-            a2 = (1/a2);
-        } 
-    }
-
-    if(pontoMedio[0] > pontoD[0]) {
-        float troca = a1;
-        a1 = a2;
-        a2 = troca;
-    }
-    
-    while (Yscan >= pontoMedio[1]) {
-        Xmin_ = (Xmin - a1);
-        Xmax_ = (Xmax - a2);
-
-        desenhaLinha(Xmin, Yscan, Xmax, Yscan, WHITE);
-
-        Yscan--;
-        Xmin = Xmin_;
-        Xmax = Xmax_;
-    }
+    pontoCima[0] = ponto1X;
+    pontoCima[1] = ponto1Y;
+    pontoMedio[0] = ponto2X;
+    pontoMedio[1] = ponto2Y;
+    pontoBaixo[0] = ponto3X;
+    pontoBaixo[1] = ponto3Y;
 }
+
